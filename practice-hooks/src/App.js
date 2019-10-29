730ea00de2c45aa142b77cefd44a39ab87812923
JorPoon <jormpoon@gmail.com>
2019-10-29 17:20:24 -0400

    adding new files

--- practice-hooks/src/App.js
+++ practice-hooks/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import Cars from './components/Cars'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Hooks</h1>
+        <Cars/>
       </header>
     </div>
   );
--- practice-hooks/src/components/Cars.js
+++ practice-hooks/src/components/Cars.js
@@ -6,3 +6,5 @@ const Cars = () => {
         <h1>Cars</h1>
     )
 }
+
+export default Cars;
