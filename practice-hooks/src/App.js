b3bc2a05252dc63023fba328169bd776d52cd758
JorPoon <jormpoon@gmail.com>
2019-11-04 23:04:06 -0500

    updating

--- practice-hooks/src/App.js
+++ practice-hooks/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Cars from './components/Cars'
 import Dogs from './components/Dogs';
+import Git from './components/Git';
 
 function App() {
   return (
@@ -9,7 +10,8 @@ function App() {
       <header className="App-header">
         <h1>Hooks</h1>
         {/* <Cars/> */}
-        <Dogs/>
+        {/* <Dogs/> */}
+        <Git/>
       </header>
     </div>
   );
