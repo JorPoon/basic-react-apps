dedd591dfc3ed7953adb9adccdf56dee8ed24dfc
JorPoon <jormpoon@gmail.com>
2019-11-06 21:27:57 -0500

    successfully retrieve github user

--- practice-hooks/src/components/Git.js
+++ practice-hooks/src/components/Git.js
@@ -1,12 +1,33 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from "axios";
 
 
 
 const Git = () => {
+    const [user, setUser] = useState([])
+
+    const getUser = () => {
+        axios.get("https://api.github.com/users/JorPoon")
+        .then(res => {
+            console.log(res.data)
+            setUser(res.data)
+        })
+    }
+
+    useEffect(() => {
+        // axios.get("https://api.github.com/users/JorPoon")
+        // .then(res => {
+        //     console.log(res.data)
+        //     setUser(res.data)
+        // })
+        getUser()
+    }, [])
     return (
+        <>
         <h1>Git Card</h1>
-        <h2>Name</h2>
+        <h2>{user.login}</h2>
         <p>profile url</p>
+        </>
     )
 }
 
